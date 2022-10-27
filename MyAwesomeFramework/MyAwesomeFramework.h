346e422e39901ea5a88d9554e6bfd6168053f9e7
Ahmad Maulana <ahmad.maulana@mekari.com>
2022-10-27 10:31:14 +0700

    First commit

---
+++ MyAwesomeFramework/MyAwesomeFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyAwesomeFramework.h
+//  MyAwesomeFramework
+//
+//  Created by Ahmad Maulana on 27/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyAwesomeFramework.
+FOUNDATION_EXPORT double MyAwesomeFrameworkVersionNumber;
+
+//! Project version string for MyAwesomeFramework.
+FOUNDATION_EXPORT const unsigned char MyAwesomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAwesomeFramework/PublicHeader.h>
+
+
---
+++ MyAwesomeFramework/MyAwesomeFrameworkFacade.swift
@@ -0,0 +1,17 @@
+//
+//  MyAwesomeFrameworkFacade.swift
+//  MyAwesomeFramework
+//
+//  Created by Ahmad Maulana on 27/10/22.
+//
+
+import Foundation
+
+public struct MyAwesomeFrameworkFacade {
+    
+    public init() {}
+    
+    public func getMyName() -> String {
+        return "Amad"
+    }
+}
